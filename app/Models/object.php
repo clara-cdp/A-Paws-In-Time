ce70cf54aa554bc84aeea90ad396c09a109ba979
clara-cdp <clarianne.cdp@gmail.com>
2026-02-22 22:22:36 +0100

    created and filled migrations, created models and seeders

---
+++ app/Models/object.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class object extends Model
+{
+    /** @use HasFactory<\Database\Factories\ObjectFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2026_02_22_130000_create_events_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->integer('step_number')->default(0); // To track story progress
+            $table->string('verb_trigger', 45); 
+        
+            // -- trigger -- (not tiggers!)      
+            $table->foreignId('object_id')->constrained(); //the IO in the room
+            $table->foreignId('required_object_id')->nullable()->constrained('objects'); // the PO in a pocket
+
+            // --unlocks: ---
+            // -> if IO is used -> sets visibility to true
+            // -> if PO is used -> sets visibility to true & sets room to null (pocket)
+            $table->foreignId('unlocked_object_id')->nullable()->constrained('objects'); 
+
+            // -> if PO is used -> unlocks next room 
+            $table->foreignId('target_room_id')->nullable()->constrained('rooms');
+            $table->integer('advances_story')->default(0);
+
+          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
