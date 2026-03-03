5e06f7434cd267914bf1101bd0bc8f096909919e
clara-cdp <clarianne.cdp@gmail.com>
2026-03-03 12:40:43 +0100

    created pockets_items table that will allow more than one user to play the game

--- database/migrations/2026_03_03_093927_create_pocket_items_table.php
+++ database/migrations/2026_03_03_093927_create_pocket_items_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pockets', function (Blueprint $table) {
+        Schema::create('pocket_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('player_id')->constrained()->onDelete('cascade'); // owner ->  can't play without a pocket
+            $table->foreignId('pocket_id')->constrained()->onDelete('cascade');
+            $table->foreignId('item_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pockets');
+        Schema::dropIfExists('pocket_items');
     }
 };
